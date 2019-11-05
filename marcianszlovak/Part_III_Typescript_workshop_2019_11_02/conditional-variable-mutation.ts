f306178d628655aa764811cd69d1dca89d9595f0
Szlovák Marcián <user.name>
2019-11-05 20:59:26 +0100

    asd

--- marcianszlovak/Part_III_Typescript_workshop_2019_11_02/conditional-variable-mutation.ts
+++ marcianszlovak/Part_III_Typescript_workshop_2019_11_02/conditional-variable-mutation.ts
@@ -1,60 +1,44 @@
-let a : number = 24;
-let out : number = 0;
+let a: number = 24;
+let out: number = 0;
 
 if (a % 2 === 0) {
-
-    ++out;
-    console.log(out);
-
+  ++out;
+  console.log(out);
 }
 
-let b : number = 13;
-let out2 : string = "";
+let b: number = 13;
+let out2: string = "";
 
 if (b > 10 && b < 20) {
-
-    out2 = "Sweet!"
-
+  out2 = "Sweet!";
 } else if (b < 10) {
-
-    out2 = "Less"
-
+  out2 = "Less";
 } else if (b < 20) {
-
-    out2 = "More"
-
+  out2 = "More";
 }
 
-let c : number = 123;
-let credits : number = 100;
-let isBonus : boolean = false;
+let c: number = 123;
+let credits: number = 100;
+let isBonus: boolean = false;
 
 if (credits >= 50 && isBonus === false) {
-
-    c = c - 1
-
+  c = c - 1;
 } else if (credits < 50 && isBonus === false) {
-
-    c = c - 1
-
+  c = c - 1;
 }
 
-let d : number = 8;
-let time : number = 120;
-let out3 : string = "";
+let d: number = 8;
+let time: number = 120;
+let out3: string = "";
 
 if (d % 4 === 0 && time <= 200) {
-
-    out3 = "check";
-    console.log(out3);
+  out3 = "check";
+  console.log(out3);
 }
 
 if (time > 200) {
-
-    out3 = "Time out";
-
+  out3 = "Time out";
 } else {
-
-    out3 = "Run Forest Run!";
-    console.log(out3);
+  out3 = "Run Forest Run!";
+  console.log(out3);
 }
--- marcianszlovak/Part_II_Typescript_Javascript/2019_09_29_matreview.ts
+++ marcianszlovak/Part_II_Typescript_Javascript/2019_09_29_matreview.ts
@@ -1,29 +1,29 @@
-console.log('hello world')
-let className = "Giggle"
-let isTuesday = true
-let greeting = "hello"
+console.log("hello world");
+let className = "Giggle";
+let isTuesday = true;
+let greeting = "hello";
 
-console.log("hello giggle")
-console.log(greeting + className)
-let magicNumber = 16
-console.log(11 % 5 === 0)
+console.log("hello giggle");
+console.log(greeting + className);
+let magicNumber = 16;
+console.log(11 % 5 === 0);
 
-let cat: string
-console.log(cat)
-cat = null // ennek a változónak nincs értéke
-const any = 7 // véletlen se változtassam meg az értéket, let-et meg tudom változtatni később
+let cat: string;
+console.log(cat);
+cat = null; // ennek a változónak nincs értéke
+const any = 7; // véletlen se változtassam meg az értéket, let-et meg tudom változtatni később
 
-let amount = 0
-amount++ // megnöveli a változó értékét eggyel
-console.log(amount)
-  ++amount
+let amount = 0;
+amount++; // megnöveli a változó értékét eggyel
+console.log(amount);
+++amount;
 
 // how to parse string to number
-let isSixEven = 6 % 2 === 0
+let isSixEven = 6 % 2 === 0;
 
 if (isSixEven) {
 }
 
 if (0) {
-  console.log("valami")
+  console.log("valami");
 }
