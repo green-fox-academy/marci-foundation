69e9b653071fe5aaad71a441ece38eb3bea82bbb
Szlovák Marcián <user.name>
2019-11-16 15:58:27 +0100

    practice

---
+++ marcianszlovak/Arrays_Functions_2019_11_09/greet.ts
@@ -0,0 +1,11 @@
+// -  Create variable named `nameToGreet` and assign the value `Green Fox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Green Fox`
+// -  Greet `nameToGreet`
+
+let nameToGreet = "Green Fox";
+function greet(name: string) {
+  console.log("Hello, " + name + "!");
+}
+
+greet(nameToGreet);
---
+++ marcianszlovak/Practice_2019_11_16/unique.ts
@@ -0,0 +1,16 @@
+//  Create a function that takes a list of numbers as a parameter
+//  Returns a list of numbers where every number in the list occurs only once
+
+// const macskaszem = [1, 2, 3, 4, 5, 5, 23, 3];
+  function cicafül([]) {
+  const macskaszem = [1, 2, 3, 4, 5, 5, 23, 3];
+  const kiscica = [...new Set(macskaszem)];
+  console.log(kiscica);
+}
+
+cicafül([]); 
+
+/* let kiscica = [1,2,3,3,4,5,5,5];
+let kiscica2 = new Set(kiscica);
+
+console.log(kiscica2); */
