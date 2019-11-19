a88f62c2a6fc013d119cc67b1bc395990c7cddc3
Marcián <marcianszlovak@gmail.com>
2019-11-19 21:42:27 +0100

    abort

--- marcianszlovak/Practice_2019_11_16/sum.ts
+++ marcianszlovak/Practice_2019_11_16/sum.ts
@@ -1,4 +1,5 @@
 // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
 
-const NumberZ = [1 , 2];
+const NumberZ = [1, 2];
 const sum = NumberZ.reduce((a, b) => a + b, 0);
+
