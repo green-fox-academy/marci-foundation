81b36eb695792a65b3dd92317999c2beb74450b1
Marcián <marcianszlovak@gmail.com>
2019-12-02 19:51:08 +0100

    triangle drawn with foreach instead of for

--- marcianszlovak/2019_11_02_Typescript_workshop/draw-triangle.ts
+++ marcianszlovak/2019_11_02_Typescript_workshop/draw-triangle.ts
@@ -1,7 +1,14 @@
-let lineCount = 4;
-let üres = "";
-
-for (let i: number = 0; i <= lineCount; i++) {
+/*for (let i: number = 0; i <= lineCount; i++) {
   üres = üres + "*";
-  console.log(üres);
+
+  let lineCount = 4;
+let üres = "";
 }
+ */
+
+let stars = [...new Array(3)];
+let star: string = "*";
+stars.forEach(element => {
+  star = star + "*";
+  console.log(star);
+});
