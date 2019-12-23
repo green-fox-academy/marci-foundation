649a2319d9dc0ed20fac03a82071c1df2232412d
Marcián <marcianszlovak@gmail.com>
2019-12-23 19:26:00 +0100

    further styleguide updates

--- marcianszlovak/2019_11_26&&30_File_IO/copy-file.ts
+++ marcianszlovak/2019_11_26&&30_File_IO/copy-file.ts
@@ -2,7 +2,7 @@
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
 
-const fs = require("fs");
+import fs from "fs";
 
 function fileCopy(fileName1: string, fileName2: string) {
   try {
