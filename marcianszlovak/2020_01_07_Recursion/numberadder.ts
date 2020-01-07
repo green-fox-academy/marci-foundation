5c55efdb915a55a9e8c0863f76e8c84767203bb1
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-07 19:50:54 +0100

    numberadder

--- marcianszlovak/2020_01_07_Recursion/numberadder.ts
+++ marcianszlovak/2020_01_07_Recursion/numberadder.ts
@@ -2,7 +2,7 @@ function addANumber(n: number) {
   if (n === 1) {
     return 1;
   } else {
-    return n + addANumber(n - 1); // 7 + 7 - 1
+    return n + addANumber(n - 1); // 7 + 6 + 5 + 4 + 3 + 2 + 1
   }
 }
 
