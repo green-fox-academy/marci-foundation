bab25ccd77d79e72a4238c132ff0c995446ebf32
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-11 12:15:02 +0100

    person and student

---
+++ marcianszlovak/2020_01_11_Class_Inheritance/inheritance/person.ts
@@ -0,0 +1,26 @@
+class Person {
+  name: string;
+  age: number;
+  gender: string;
+  constructor(
+    name: string = "Jane Doe",
+    age: number = 30,
+    gender: string = "female"
+  ) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  introduce() {
+    "Hi, I'm " +
+      this.name +
+      ", a " +
+      this.age +
+      " year old " +
+      this.gender +
+      ".";
+  }
+  getGoal() {
+    console.log("Live for the moment");
+  }
+}
---
+++ marcianszlovak/2020_01_11_Class_Inheritance/inheritance/student.ts
@@ -0,0 +1,6 @@
+import { Person } from "./person";
+
+export class Student extends Person {
+  previousOrganization: string;
+  skippedDays: number;
+}
