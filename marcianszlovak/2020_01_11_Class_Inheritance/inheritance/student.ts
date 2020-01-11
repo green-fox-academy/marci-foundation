d6d9d6f0e4744f361a1e41e29a0bd23fb39f4150
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-11 12:17:52 +0100

    person and student

--- marcianszlovak/2020_01_11_Class_Inheritance/inheritance/student.ts
+++ marcianszlovak/2020_01_11_Class_Inheritance/inheritance/student.ts
@@ -3,4 +3,15 @@ import { Person } from "./person";
 export class Student extends Person {
   previousOrganization: string;
   skippedDays: number;
+  constructor(
+    name: string,
+    gender: string,
+    age: number,
+    previousOrganization: string,
+    skippedDays: number
+  ) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = skippedDays;
+  }
 }
