c22095e1e74c3f84c1086acf1331807d6b364363
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-14 19:50:50 +0100

    plants reworked

--- marcianszlovak/2020_01_11_14_Class_Inheritance/garden/Plants.ts
+++ marcianszlovak/2020_01_11_14_Class_Inheritance/garden/Plants.ts
@@ -1,28 +1,26 @@
 export class Plants {
-  currentWaterLevel: number;
+  currentWaterLevel: number = 0;
   needWater: number;
   absorbAmount: number;
-  waterAdded: number;
   color: string;
 
   constructor(
     currentWaterLevel: number,
     needWater: number,
     absorbAmount: number,
-    waterAdded: number,
     color: string
   ) {
     (this.currentWaterLevel = currentWaterLevel),
       (this.needWater = needWater),
       (this.absorbAmount = absorbAmount),
-      (this.waterAdded = waterAdded),
       (this.color = color);
   }
-  isWaterNeeded() {
-    if (this.currentWaterLevel < this.needWater) {
-      // needs more water
-    } else {
-      // doesn't need more water
-    }
+
+  wateringPlants(a: number) {
+    this.currentWaterLevel += a * this.absorbAmount;
+  }
+
+  plantNeedsWater(): boolean {
+    return this.currentWaterLevel > this.needWater ? false : true;
   }
 }
