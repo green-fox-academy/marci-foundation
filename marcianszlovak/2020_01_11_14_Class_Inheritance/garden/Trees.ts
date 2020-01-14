fa2a80e6bd04d9d6944bbf6af08a2d7022ef4050
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-14 20:02:27 +0100

    trees updated

--- marcianszlovak/2020_01_11_14_Class_Inheritance/garden/Trees.ts
+++ marcianszlovak/2020_01_11_14_Class_Inheritance/garden/Trees.ts
@@ -1,13 +1,9 @@
 import { Plants } from "./Plants";
 
 export class Trees extends Plants {
-  constructor(
-    currentWaterLevel: number,
-    needWater: number,
-    absorbAmount: number,
-    waterAdded: number,
-    color: string
-  ) {
-    super(0, 10, 0.4, waterAdded, "purple");
+  constructor(currentWaterLevel: number, color: string) {
+    super(currentWaterLevel, color);
+    this.needWater = 10;
+    this.absorbAmount = 0.4;
   }
 }
