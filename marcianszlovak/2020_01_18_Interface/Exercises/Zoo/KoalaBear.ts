cc5bd550ebfe2517ea740dae3ed798d59e3d2239
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-18 13:55:06 +0100

    koala bear

---
+++ marcianszlovak/2020_01_18_Interface/Exercises/Zoo/KoalaBear.ts
@@ -0,0 +1,24 @@
+import Animals from "./Animals";
+export default class KoalaBear extends Animals {
+  nativeCountry: string;
+  herbivorous: boolean;
+  constructor(
+    name: string = "Koala Bear",
+    age: number = 7,
+    legs: number = 4,
+    nativeCountry: string = "Australia",
+    herbivorous: boolean = true
+  ) {
+    super(name, age, legs);
+    this.nativeCountry = nativeCountry;
+    this.herbivorous = herbivorous;
+  }
+
+  breed(): string {
+    return "9 times a year";
+  }
+
+  eat(): string {
+    return "bamboo leaves";
+  }
+}
