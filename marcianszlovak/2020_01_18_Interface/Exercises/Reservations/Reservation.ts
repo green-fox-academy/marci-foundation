a873095be9872a5bd0db532446ecfc1e210cb3c5
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-18 16:21:29 +0100

    getcode,dow

--- marcianszlovak/2020_01_18_Interface/Exercises/Reservations/Reservation.ts
+++ marcianszlovak/2020_01_18_Interface/Exercises/Reservations/Reservation.ts
@@ -2,4 +2,17 @@
 
 import Reservationy from "./Reservationy";
 
-export default class Reservation implements Reservationy {}
+export default class Reservation implements Reservationy {
+  code: string;
+  dow: string;
+
+  constructor() {}
+
+  getCodeBooking(): string {
+    return;
+  }
+
+  getDowBooking(): string {
+    return;
+  }
+}
