355b523f4099063416d919bc29730a74eb1b9ff2
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-21 19:33:07 +0100

    write multiple lines second solution

--- marcianszlovak/2019_11_26&&30_File_IO/write-multiple-lines2.ts
+++ marcianszlovak/2019_11_26&&30_File_IO/write-multiple-lines2.ts
@@ -4,6 +4,10 @@ let writer = fs.createWriteStream("log2.txt", {
   flags: "a"
 });
 
-writer.write("valami \n");
-writer.write("valami1 \n");
-writer.write("valami2 \n");
+writer.write("valami \n", "valami2");
+
+writer.on("finish", () => {
+  console.log("wrote all data to the text file");
+});
+
+writer.end();
