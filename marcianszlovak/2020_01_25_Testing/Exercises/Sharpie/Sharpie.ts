c90d1da4ab5361db109f27de1ff1557e0ad166eb
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 15:34:55 +0100

    Sharpie p1

--- marcianszlovak/2020_01_25_Testing/Exercises/Sharpie/Sharpie.ts
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-import test from "tape";
-import Sharpie from "../../../2019_12_14_Classes/class exercises/sharpie";
---
+++ marcianszlovak/2020_01_25_Testing/Exercises/Sharpie/Sharpie_test.ts
@@ -0,0 +1,4 @@
+"use strict";
+
+import Sharpie from "../../../2019_12_14_Classes/class exercises/sharpie";
+import test from "tape";
