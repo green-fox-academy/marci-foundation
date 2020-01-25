ad69cadb24eeb3beae8add0e808653c53173989f
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 16:24:02 +0100

    animals testing p1

--- marcianszlovak/2020_01_25_Testing/Exercises/Animal/Animal_test.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/Animal/Animal_test.ts
@@ -2,3 +2,7 @@
 
 import test from "tape";
 import Animal from "../../../2019_12_14_Classes/class exercises/animal";
+
+test("someone is eating, drinking, playing...", t => {
+  const animal = new Animal();
+});
