254c78ee21a9e3bd534911ae44963eddd4f72954
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 14:30:25 +0100

    counting letters test p2

--- marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters.ts
@@ -1,20 +1,16 @@
 "use strict";
 
 export default function countLetters(dictionary: string) {
-  if (dictionary === undefined) {
-    return undefined;
-  } else {
-    let letters = {};
+  let letterFrequency = {};
 
-    for (let index = 0; index < dictionary.length; index++) {
-      let modifiedLetters = dictionary.charAt(index).toUpperCase();
+  for (let index = 0; index < dictionary.length; index++) {
+    const element = dictionary.charAt(index);
 
-      if (letters[modifiedLetters]) {
-        letters[modifiedLetters]++;
-      } else {
-        letters[modifiedLetters] = 1;
-      }
+    if (letterFrequency[element]) {
+      letterFrequency[element]++;
+    } else {
+      letterFrequency[element] = 1;
     }
-    return letters;
+    return letterFrequency;
   }
 }
--- marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters_test.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters_test.ts
@@ -2,10 +2,3 @@
 
 import countLetters from "./countLetters";
 import test from "tape";
-
-test("Counting letters", t => {
-  const actual = countLetters("supreme");
-  const expected = 2;
-  t.equal(actual, expected);
-  t.end();
-});
