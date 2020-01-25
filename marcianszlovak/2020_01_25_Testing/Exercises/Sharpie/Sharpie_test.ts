c7b0b364971449ae105b29fd4cc504f955a80dd8
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 15:44:21 +0100

    sharpie test p2

--- marcianszlovak/2020_01_25_Testing/Exercises/Sharpie/Sharpie_test.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/Sharpie/Sharpie_test.ts
@@ -2,3 +2,9 @@
 
 import Sharpie from "../../../2019_12_14_Classes/class exercises/sharpie";
 import test from "tape";
+
+test("using sharpie", t => {
+  const operations = new Sharpie("green", 770);
+  let actual = ;
+  let expected = 
+});
