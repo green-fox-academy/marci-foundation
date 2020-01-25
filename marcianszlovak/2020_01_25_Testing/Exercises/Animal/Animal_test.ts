f34bc0d4b428f8afe6560bd802e3bfa258fb5981
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 16:35:32 +0100

    animal test with eating + drinking

--- marcianszlovak/2020_01_25_Testing/Exercises/Animal/Animal_test.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/Animal/Animal_test.ts
@@ -3,6 +3,15 @@
 import test from "tape";
 import Animal from "../../../2019_12_14_Classes/class exercises/animal";
 
-test("someone is eating, drinking, playing...", t => {
+test("someone is eating, drinking...", t => {
   const animal = new Animal();
+
+  animal.eat();
+  animal.drink();
+
+  t.equal(animal.hunger, 49, "eating");
+  t.equal(animal.thirst, 22, "drinking");
+  t.end();
 });
+
+test("someone is playing, I wonder who...", t => {});
