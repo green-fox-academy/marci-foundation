e1f978c13b4953d179028a2920fc32203db25293
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 15:13:44 +0100

    fibonacci test p1

--- marcianszlovak/2020_01_25_Testing/Exercises/Fibonacci/fibonacci_test.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/Fibonacci/fibonacci_test.ts
@@ -2,3 +2,11 @@
 
 import fibonacci from "./fibonacci";
 import test from "tape";
+
+test("testing the fibonacci sequence", t => {
+  const actual = fibonacci(4);
+  const expected = 9;
+
+  t.equal(actual, expected);
+  t.end();
+});
