392b1822a40f74ef640f486746553c628a2d4812
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 14:51:05 +0100

    counting letters test p3

--- marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters.ts
@@ -1,16 +1,9 @@
 "use strict";
 
-export default function countLetters(dictionary: string) {
-  let letterFrequency = {};
-
-  for (let index = 0; index < dictionary.length; index++) {
-    const element = dictionary.charAt(index);
-
-    if (letterFrequency[element]) {
-      letterFrequency[element]++;
-    } else {
-      letterFrequency[element] = 1;
-    }
-    return letterFrequency;
-  }
+export default function count(dictionary: string) {
+  let count = {};
+  dictionary.split("").forEach(currentItem => {
+    count[currentItem] ? count[currentItem]++ : (count[currentItem] = 1);
+  });
+  return count;
 }
--- marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters_test.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/CountLetters/countLetters_test.ts
@@ -3,10 +3,11 @@
 import countLetters from "./countLetters";
 import test from "tape";
 
+let a: string = "supreme";
 test("Counting letters in a string", t => {
-  const actual = countLetters("supreme");
+  const actual = countLetters(a);
   const expected = { e: 2 };
 
-  t.strictEqual(actual, expected);
+  t.deepEqual(actual, expected);
   t.end();
 });
