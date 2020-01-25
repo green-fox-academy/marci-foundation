200f474e965c8a74b53331f6dc16cc1476667d10
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 12:25:14 +0100

    anagram tests

--- marcianszlovak/2020_01_25_Testing/Exercises/Anagrams/Anagrams_test.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/Anagrams/Anagrams_test.ts
@@ -1,4 +1,20 @@
 "use strict";
 
 import Anagrams from "./Anagrams";
-import tape from "tape";
+import test from "tape";
+
+test("Is it an anagram?", t => {
+  const actual = Anagrams("cat", "cta");
+  const expected = true;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test("Is THIS an anagram?", t => {
+  const actual = Anagrams("no, it's not", "faaös9d8f7fdjfdjdsafnaűáalsdf");
+  const expected = true;
+
+  t.equal(actual, expected);
+  t.end();
+});
