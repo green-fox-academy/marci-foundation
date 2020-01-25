ecf72013b393804013b6f3895460bd873c24ce50
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-25 11:38:23 +0100

    sum reworked

--- marcianszlovak/2020_01_25_Testing/Exercises/Sum/Sum.ts
+++ marcianszlovak/2020_01_25_Testing/Exercises/Sum/Sum.ts
@@ -3,10 +3,10 @@ export default class Add {
    * sum
    */
   public sum(...inputNumber: number[]) {
-    return inputNumber.reduce((sum, array) => sum + array, 0);
+    return inputNumber.reduce((sum, inputNumber) => sum + inputNumber, 0);
   }
 }
 
-let asd = new Add();
-asd.sum(4, 5, 6, 7, 6);
-console.log(asd.sum(4, 5, 6, 7, 6));
+let summed = new Add();
+summed.sum(4, 5, 6, 7, 6);
+console.log(summed.sum(4, 5, 6, 7, 6));
